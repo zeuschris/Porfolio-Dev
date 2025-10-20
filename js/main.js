578dbfca4dcb0bfc6445591b854815d99132ceed
Christopher Montes <zeuschris123@gmail.com>
2025-10-20 11:33:03 -0300

    porfolio 1.0

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll(".fade-in");
+
+  const appearOptions = { threshold: 0.2 };
+  const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add("visible");
+      observer.unobserve(entry.target);
+    });
+  }, appearOptions);
+
+  faders.forEach(fade => {
+    appearOnScroll.observe(fade);
+  });
+});
